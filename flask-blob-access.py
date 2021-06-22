9da0736ffd088bae01d24a628f2dd198f4d2f4fc
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2021-06-22 13:11:42 +0200

    read_with_ds now accepts subfolder and arrow files

--- flask-blob-access.py
+++ flask-blob-access.py
@@ -4,11 +4,13 @@ import os
 import io
 import time
 import datetime
+from pathlib import Path
 
 from flask import Flask
 import pyarrow as pa
 import pyarrow.parquet as pq
 import pyarrow.dataset
+import pyarrow.feather
 from azure.storage.blob import ContainerClient
 import fsspec
 import adlfs
@@ -22,6 +24,7 @@ if not BLOB_ACCOUNT_NAME or not BLOB_ACCOUNT_KEY:
 
 
 
+#logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(name)s - %(pathname)s:%(lineno)s - %(levelname)s - %(message)s" , datefmt="%H:%M:%S")
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s" , datefmt="%H:%M:%S")
 logging.getLogger("azure.core.pipeline.policies.http_logging_policy").setLevel(logging.WARNING)
 
@@ -33,10 +36,14 @@ def entry_point():
     return f"ALIVE --- {datetime.datetime.now()}"
 
 
-@app.route("/read_with_ds/<filename>")
-def read_with_ds(filename: str):
+@app.route("/read_with_ds/<filename>",  defaults={"subfolder": None})
+@app.route("/read_with_ds/<subfolder>/<filename>")
+def read_with_ds(subfolder: str, filename: str):
 
-    file_path = "access_test/" + filename
+    if subfolder is not None:
+        file_path = f"access_test/{subfolder}/{filename}"
+    else:
+        file_path = f"access_test/{filename}"
 
     app.logger.info(f"executing read_with_ds() against '{file_path}'")
     
@@ -44,9 +51,20 @@ def read_with_ds(filename: str):
 
     blob_path = "abfs://test-container-laget-av-sigurd/" + file_path
     fs = fsspec.filesystem(protocol='abfs', account_name=BLOB_ACCOUNT_NAME, account_key=BLOB_ACCOUNT_KEY)
-    ds = pa.dataset.dataset(blob_path, filesystem=fs, format="parquet")
 
-    table = ds.to_table(columns=["REAL", "DATE", "FGIP"])
+    if Path(filename).suffix == ".parquet":
+        ds = pa.dataset.dataset(blob_path, filesystem=fs, format="parquet")
+    else:
+        ds = pa.dataset.dataset(blob_path, filesystem=fs, format="arrow")
+
+    columns_to_get = ["DATE", "FGIP"]
+    if "REAL" in ds.schema.names:
+        columns_to_get = ["REAL", *columns_to_get]
+
+    app.logger.info("executing ds.to_table()")
+    table = ds.to_table(columns=columns_to_get)
+    app.logger.info("finished ds.to_table()")
+
     df = table.to_pandas()
 
     elapsed_s = time.perf_counter() - start_s
@@ -59,6 +77,7 @@ def read_with_ds(filename: str):
     retstr += f"<br>shape={table.shape}"
     retstr += "<br><br>"
     retstr += table.to_pandas().head().to_html()
+    retstr += table.to_pandas().tail().to_html()
 
     return retstr
 
@@ -94,6 +113,69 @@ def download_and_read(filename: str):
     retstr += table.to_pandas().head().to_html()
 
     return retstr
+
+
+@app.route("/read_arrow/<filename>",  defaults={"subfolder": None})
+@app.route("/read_arrow/<subfolder>/<filename>")
+def read_arrow(subfolder: str, filename: str):
+
+    if subfolder is not None:
+        file_path = f"access_test/{subfolder}/{filename}"
+    else:
+        file_path = f"access_test/{filename}"
+
+    app.logger.info(f"executing read_arrow() against '{file_path}'")
+    
+    start_s = time.perf_counter()
+
+    blob_path = "abfs://test-container-laget-av-sigurd/" + file_path
+    fs = fsspec.filesystem(protocol='abfs', account_name=BLOB_ACCOUNT_NAME, account_key=BLOB_ACCOUNT_KEY)
+
+    thefile = fs.open(blob_path, "rb")
+
+    app.logger.info("============================")
+    app.logger.info("about to open file")
+    reader = pyarrow.ipc.RecordBatchFileReader(thefile)
+
+    app.logger.info(f"reader.num_record_batches={reader.num_record_batches}")
+    app.logger.info(f"reader.stats={reader.stats}")
+
+    app.logger.info("getting column names")
+    column_names_on_file = reader.schema.names
+
+    columns_to_get = ["DATE", "FGIP"]
+    if "REAL" in column_names_on_file:
+        columns_to_get = ["REAL", *columns_to_get]
+
+    app.logger.info("read_all")
+    table=reader.read_all().select(columns_to_get)
+
+    #pa.feather.read_table(thefile, columns=["DATE", "FGIP"], memory_map=False)
+
+    app.logger.info(f"reader.stats={reader.stats}")
+    app.logger.info("done")
+    app.logger.info("============================")
+
+
+    app.logger.info("topandas")
+    df = table.to_pandas()
+
+    elapsed_s = time.perf_counter() - start_s
+
+    app.logger.info(f"finished read_arrow() in {elapsed_s:.3f}s, table.shape: {table.shape}")
+
+    retstr = f"DONE - {file_path}"
+    retstr += "<br>"
+    retstr += f"<br>elapsed_s={elapsed_s:.3f}"
+    retstr += f"<br>shape={table.shape}"
+    retstr += "<br><br>"
+    retstr += table.to_pandas().head().to_html()
+    retstr += table.to_pandas().tail().to_html()
+
+    return retstr
+
+
+
 
 
 if __name__ == '__main__':
